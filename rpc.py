ea6a65165eb13154a124acf5ab80271b1ef96c81
kseiru <kseiru@knteam.ru>
2022-03-07 03:08:42 +0300

    im dumb sooo

--- rpc.py
+++ rpc.py
@@ -1,10 +1,44 @@
-from flask import Flask
+from pypresence import Presence
+from config import API_KEY, BRAWL_TAG, client_id
+import json
+from urllib.parse import quote
+import requests
+import time
+from datetime import datetime
 
-app = Flask(__name__)
+if "#" not in BRAWL_TAG:
+    tag = f"#{BRAWL_TAG}"
+else:
+    tag = BRAWL_TAG
 
-@app.route('/api')
-def main_page():
-    return "Welcome to the unofficial brawl stars API!\nYou will not need an API key here!"
+tag = quote(tag)
 
-if __name__ == "__main__":
-    app.run(host="81.177.165.173", port=80)
+
+headers = {
+    "Authorization": f"Bearer {API_KEY}"
+}
+
+get_player_data = requests.get(url=f"https://api.brawlstars.com/v1/players/{tag}", headers=headers)
+data = get_player_data.content
+data = json.loads(data)
+
+username = data["name"]
+trophies = data["trophies"]
+brawlers = data["brawlers"]
+icon = data["icon"]["id"]
+
+def sort_trophies(x):
+    return x["trophies"]
+
+brawlers.sort(key=sort_trophies, reverse=True)
+top_brawler = brawlers[0]
+
+top_brawler_name = top_brawler["name"]
+top_brawler_trophies = top_brawler["trophies"]
+
+RPC = Presence(client_id)
+RPC.connect()
+RPC.update(large_image=f"https://kseiru.ru/brawl/{icon}.png", details=f"{username} - {trophies} 🏆", state=f"Top brawler: {top_brawler_name} - {top_brawler_trophies} 🏆", large_text="Brawl Stars", start=int(datetime.now().timestamp()))
+
+while True:
+    time.sleep(15)
